b14fab6c96ebdec679c682e90063ac655c31cce2
Albert Chae <albertchae@users.noreply.github.com>
2024-03-02 14:44:06 -0800

    remove old stuff from mkYarnPackage

--- pkgs/by-name/atomic-browser/package.nix
+++ pkgs/by-name/atomic-browser/package.nix
@@ -12,14 +12,4 @@ mkPnpmPackage rec {
   version = "v0.37.0";
   src = "${source}/browser";
 
-  installPhase = "";
-  distPhase = "";
-
-   packageJSON = "${source}/browser/package.json";
-   # Upstream does not contain a yarn.lock
-   yarnLock = ./yarn.lock;
-   offlineCache = fetchYarnDeps {
-     yarnLock = ./yarn.lock;
-     hash = "sha256-GK5Ehk82VQ5ajuBTQlPwTB0aaxhjAoD2Uis8wiam7Z0=";
-   };
 }
