e535d7c278e4f625220f495b887cd7583c594e17
Anish Lakhwara <anish+git@lakhwara.com>
2024-03-03 18:32:56 -0800

    point atomic-browser to our fork

--- pkgs/by-name/atomic-browser/package.nix
+++ pkgs/by-name/atomic-browser/package.nix
@@ -1,7 +1,7 @@
 { pkgs, fetchFromGitHub, mkPnpmPackage, fetchYarnDeps, fetchpatch, ... }:
 let
   source = fetchFromGitHub {
-    owner = "atomicdata-dev";
+    owner = "ngi-nix";
     repo = "atomic-server";
     rev = "v0.37.0";
     hash = "sha256-+Lk2MvkTj+B+G6cNbWAbPrN5ECiyMJ4HSiiLzBLd74g=";
@@ -12,4 +12,5 @@ mkPnpmPackage rec {
   version = "v0.37.0";
   src = "${source}/browser";
 
+  extraBuildInputs = [pkgs.typescript];
 }
